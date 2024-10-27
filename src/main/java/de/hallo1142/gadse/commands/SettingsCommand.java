0a0ccbe2c1109f861be1f2c8ea9970e79086cbf7
hallo1142 <mail@hallo1142.de>
2024-10-27 22:10:47 +0100

    Begin slash command handling

---
+++ src/main/java/de/hallo1142/gadse/commands/SettingsCommand.java
@@ -0,0 +1,4 @@
+package de.hallo1142.gadse.commands;
+
+public class SettingsCommand {
+}
---
+++ src/main/java/de/hallo1142/gadse/events/SlashCommandEvent.java
@@ -0,0 +1,12 @@
+package de.hallo1142.gadse.events;
+
+import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+
+public class SlashCommandEvent extends ListenerAdapter {
+
+    @Override
+    public void onSlashCommandInteraction(SlashCommandInteractionEvent event) {
+        super.onSlashCommandInteraction(event);
+    }
+}

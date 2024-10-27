929ddf4e0a1939d11f9257402d623e8f5f487e17
Kevin-Damian Gosa <kevingosa@zyonicsoftware.com>
2024-10-27 22:02:50 +0100

    first commit

---
+++ src/main/java/de/hallo1142/gadse/DiscordBot.java
@@ -0,0 +1,64 @@
+package de.hallo1142.gadse;
+
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions;
+import net.dv8tion.jda.api.interactions.commands.OptionType;
+import net.dv8tion.jda.api.interactions.commands.build.Commands;
+import net.dv8tion.jda.api.interactions.commands.build.SubcommandData;
+
+public class DiscordBot {
+
+    public DiscordBot() {
+        JDA api = JDABuilder
+                .createDefault(System.getenv("JDA_TOKEN"))
+                .setActivity(Activity.watching("Katze"))
+                .build();
+        this.registerCommands(api);
+    }
+
+    private void registerCommands(JDA jda) {
+        jda.updateCommands().addCommands(
+                Commands.slash("allianceadmin", "Verwaltung für Allianzen")
+                        .addSubcommands(
+                                new SubcommandData("create", "Erstellt eine neue Allianz")
+                                        .addOption(OptionType.STRING, "name", "Name der Allianz", true),
+                                new SubcommandData("delete", "Löscht eine Allianz")
+                                        .addOption(OptionType.STRING, "name", "Name der Allianz", true),
+                                new SubcommandData("list", "Liste der aktuellen Allianzen"),
+                                new SubcommandData("addadmin", "Fügt einer Allianz einen Admin hinzu")
+                                        .addOption(OptionType.STRING, "name", "Name der Allianz", true)
+                                        .addOption(OptionType.USER, "user", "User welcher als Admin angelegt werden soll", true),
+                                new SubcommandData("removeadmin", "Entferne einen Admin einer Allianz")
+                                        .addOption(OptionType.STRING, "name", "Name der Allianz", true)
+                                        .addOption(OptionType.USER, "user", "User welcher als Admin entfernt werden soll", true),
+                                new SubcommandData("addmember", "Fügt ein Mitglied einer Allianz hinzu")
+                                        .addOption(OptionType.STRING, "name", "Name der Allianz", true)
+                                        .addOption(OptionType.USER, "user", "User welcher als Mitglied hinzugeügt werden soll", true),
+                                new SubcommandData("removemember", "Entfernt ein Mitglied aus einer Allianz")
+                                        .addOption(OptionType.STRING, "name", "Name der Allianz", true)
+                                        .addOption(OptionType.USER, "user", "User welcher als Mitglied entfernt werden soll"),
+                                new SubcommandData("info", "Info über eine Allianz")
+                                        .addOption(OptionType.STRING, "name", "Name der Allianz", true)
+                        )
+                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.MANAGE_ROLES)),
+                Commands.slash("alliance", "User-Commands für bestehende Allianzen")
+                        .addSubcommands(
+                                new SubcommandData("addmember", "Fügt deiner Allianz ein Mitglied hinzu")
+                                        .addOption(OptionType.USER, "user", "User der hinzugefügt werden soll", true),
+                                new SubcommandData("removemember", "Entferne ein Mitglied aus deiner Allianz")
+                                        .addOption(OptionType.USER, "user", "User der entfernt werden soll", true),
+                                new SubcommandData("info", "Info über deine Allianz")
+                        ),
+                Commands.slash("settings", "Einstellungen für den Bot")
+                    .addSubcommands(
+                            new SubcommandData("alliancecategory", "Setze die Kategorie für Allianzchannel")
+                                    .addOption(OptionType.INTEGER, "id", "Kategorie-ID")
+                    )
+                        .setDefaultPermissions(DefaultMemberPermissions.enabledFor(Permission.ADMINISTRATOR))
+        ).queue();
+    }
+
+}
---
+++ src/main/java/de/hallo1142/gadse/GadseBot.java
@@ -0,0 +1,9 @@
+package de.hallo1142.gadse;
+
+public class GadseBot {
+
+    public static void main(String[] args) {
+        DiscordBot discordBot = new DiscordBot();
+    }
+
+}
